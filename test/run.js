74649f66e1ca929ec0baf2a74c6c852400596776
carbidethegreate <whitakerdavid@pm.me>
2025-07-09 08:49:32 -0400

    fix missing dependency and add test header

--- test/run.js
+++ test/run.js
@@ -1,3 +1,11 @@
+/*  OnlyFans Automation Manager
+    File: run.js
+    Purpose: simple test runner
+    Created: 2025-07-06 – v1.0
+*/
+
 import './churnPredictor.test.js';
 import './utils.test.js';
 import './secureKeys.test.js';
+
+/*  End of File – Last modified 2025-07-06 */
